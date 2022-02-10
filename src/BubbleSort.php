bf961630e615e75375b8d9346cf0acf606723ebd
Darío Rivera <fermius.us@gmail.com>
2022-02-10 16:30:00 -0500

    refactor: rename variable

--- src/BubbleSort.php
+++ src/BubbleSort.php
@@ -11,11 +11,11 @@ class BubbleSort
         for ($i=0; $i<$size -1; $i++) {
             for ($j=0; $j<$size - 1 - $i; $j++) {
                 $item = $collection[$j];
-                $nextSquare = $collection[$j+1];
+                $nextItem = $collection[$j+1];
 
-                if ($nextSquare < $item) {
+                if ($nextItem < $item) {
                     $collection[$j+1] = $item;
-                    $collection[$j] = $nextSquare;
+                    $collection[$j] = $nextItem;
                 }
             }
         }

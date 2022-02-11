0e9ab87ebff0b959b7c65714406f6e898d6276b3
Darío Rivera <fermius.us@gmail.com>
2022-02-10 19:39:46 -0500

    feat: integrate psr

--- src/BubbleSort.php
+++ src/BubbleSort.php
@@ -8,13 +8,13 @@ class BubbleSort
     {
         $size = count($collection);
 
-        for ($i=0; $i<$size -1; $i++) {
-            for ($j=0; $j<$size - 1 - $i; $j++) {
+        for ($i = 0; $i < $size - 1; $i++) {
+            for ($j = 0; $j < $size - 1 - $i; $j++) {
                 $item = $collection[$j];
-                $nextItem = $collection[$j+1];
+                $nextItem = $collection[$j + 1];
 
                 if ($nextItem < $item) {
-                    $collection[$j+1] = $item;
+                    $collection[$j + 1] = $item;
                     $collection[$j] = $nextItem;
                 }
             }
